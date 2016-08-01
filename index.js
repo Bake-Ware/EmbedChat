277e438afaa34653845303abde0e15d62902cf7a
jamixzol <jamixzol@gmail.com>
2016-08-01 04:42:22 -0500

    Super ghetto usernames

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  console.log("New connection from " + socket.request.connection.remoteAddress);
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
